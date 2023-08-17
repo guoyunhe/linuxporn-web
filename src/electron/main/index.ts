7dfe188a3ef3166090f9aaea7619c2c641e3e911
Guo Yunhe <i@guoyunhe.me>
2023-08-17 22:54:31 +0800

    custom path

--- src/electron/main/index.ts
+++ src/electron/main/index.ts
@@ -34,7 +34,7 @@ function createWindow(): void {
   if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
     mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL']);
   } else {
-    mainWindow.loadFile(path.join(__dirname, '../../../../index.html'));
+    mainWindow.loadFile(path.join(__dirname, '../../../index.html'));
   }
 }
 
